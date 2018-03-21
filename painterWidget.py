9525695b3f8019167aafaa94fa2660edf58d53e7
BakedRibs <18101325879@163.com>
2018-03-21 20:47:58 +0800

    20180321-2047

--- painterWidget.py
+++ painterWidget.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPalette, QColor
 
 class painterWidget(QWidget):
     def __init__(self):
@@ -8,3 +9,6 @@ class painterWidget(QWidget):
     def Init_UI(self):
         self.setFixedSize(500, 500)
         self.setAutoFillBackground(True)
+        pal = QPalette()
+        pal.setColor(QPalette.Background, QColor(255, 255, 255))
+        self.setPalette(pal)
