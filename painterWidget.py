a135809d8bf97d43f67e6a6af94477f90492452e
BakedRibs <18101325879@163.com>
2018-04-03 18:56:02 +0800

    20180403-1855

--- painterWidget.py
+++ painterWidget.py
@@ -29,24 +29,47 @@ class painterWidget(QWidget):
     def paintCircle(self, event, qp):
         for i in range(self.circleNumber):
             qp.setPen(QColor(0, 0, 0))
-            qp.drawEllipse(self.points[i][0], self.points[i][1], 50, 50)
+            qp.drawEllipse(self.points[i][0]-50, self.points[i][1]-50, 100, 100)
         
     def paintStatusChanged(self, count):
         self.circleNumber = count
         self.points = []
+        self.id = []
         self.connections = []
+        self.size = []
         for i in range(count):
             point = []
             point.append(round(random.uniform(0, 500), 2))
             point.append(round(random.uniform(0, 500), 2))
             self.points.append(point)
+            self.id.append(i)
+            self.size.append(1)
         for i in range(count):
             for j in range(i+1, count):
                 if self.connectToAnother(i, j, 50) == True:
                     self.connections.append([i, j])
-        for i in self.connections:
-            pass
+        for i in range(len(self.connections)):
+            self.unionRoot(self.connections[i][0], self.connections[i][1])
+            print(self.connections[i], self.id)
+        self.id.sort()
         self.update()
+        
+    def unionRoot(self, p, q):
+        i = self.findRoot(p)
+        j = self.findRoot(q)
+        if i == j:
+            return
+        if self.size[i] < self.size[j]:
+            self.id[i] = j
+            self.size[j] += self.size[i]
+        else:
+            self.id[j] = i
+            self.size[i] += self.size[j]
+        
+    def findRoot(self, p):
+        while p != self.id[p]:
+            p = self.id[p]
+        return p
         
     def connectToAnother(self, i, j, radius):
         dis2 = pow((self.points[j][0] - self.points[i][0]), 2) + pow((self.points[j][1]-self.points[i][1]), 2)
